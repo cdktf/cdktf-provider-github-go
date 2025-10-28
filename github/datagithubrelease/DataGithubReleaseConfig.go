34c68fd91ca130004f85ca7577173d8faeb85810
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2025-10-28 03:57:17 +0000

    chore(release): v15.1.1

--- github/datagithubrelease/DataGithubReleaseConfig.go
+++ github/datagithubrelease/DataGithubReleaseConfig.go
@@ -22,20 +22,20 @@ type DataGithubReleaseConfig struct {
 	Provider cdktf.TerraformProvider `field:"optional" json:"provider" yaml:"provider"`
 	// Experimental.
 	Provisioners *[]interface{} `field:"optional" json:"provisioners" yaml:"provisioners"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#owner DataGithubRelease#owner}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#owner DataGithubRelease#owner}.
 	Owner *string `field:"required" json:"owner" yaml:"owner"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#repository DataGithubRelease#repository}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#repository DataGithubRelease#repository}.
 	Repository *string `field:"required" json:"repository" yaml:"repository"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#retrieve_by DataGithubRelease#retrieve_by}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#retrieve_by DataGithubRelease#retrieve_by}.
 	RetrieveBy *string `field:"required" json:"retrieveBy" yaml:"retrieveBy"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#id DataGithubRelease#id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#id DataGithubRelease#id}.
 	//
 	// Please be aware that the id field is automatically added to all resources in Terraform providers using a Terraform provider SDK version below 2.
 	// If you experience problems setting this value it might not be settable. Please take a look at the provider documentation to ensure it should be settable.
 	Id *string `field:"optional" json:"id" yaml:"id"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#release_id DataGithubRelease#release_id}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#release_id DataGithubRelease#release_id}.
 	ReleaseId *float64 `field:"optional" json:"releaseId" yaml:"releaseId"`
-	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.0/docs/data-sources/release#release_tag DataGithubRelease#release_tag}.
+	// Docs at Terraform Registry: {@link https://registry.terraform.io/providers/integrations/github/6.7.1/docs/data-sources/release#release_tag DataGithubRelease#release_tag}.
 	ReleaseTag *string `field:"optional" json:"releaseTag" yaml:"releaseTag"`
 }
 
